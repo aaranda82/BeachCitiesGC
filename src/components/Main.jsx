b3e18b0b5ed3e8a8e8cea18c47898736fce597c6
aaranda82 <alexraranda@gmail.com>
2021-01-18 21:38:22 -0800

    Prettier/Eslint setup

---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const MainDiv = styled.h1`
+  color: red;
+  text-align: center;
+`;
+
+const Main = () => {
+  return (
+    <div>
+      <MainDiv>Beach Cities General Construction</MainDiv>
+    </div>
+  );
+};
+
+export default Main;
