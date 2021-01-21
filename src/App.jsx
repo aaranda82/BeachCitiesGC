91a712c35fb671b2ef03f24d303aaf501e93e599
aaranda82 <alexraranda@gmail.com>
2021-01-20 18:40:43 -0800

    Update homepage layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import Main from "./components/Main";
+import styled from "styled-components";
+import hero from "./assets/hero.jpeg";
+
+const Image = styled.img`
+  width: 100%;
+  height: 500px;
+  background-image: url(/static/media/hero.0e0ef208.jpeg);
+  background-size: cover;
+  background-attachment: fixed;
+`;
+
+const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Div>
+      <Image id="App" />
       <Main />
-    </div>
+    </Div>
   );
 }
 
