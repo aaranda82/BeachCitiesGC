7ec752c1c7ac204a81d2287f05d5e71f6307ce8d
aaranda82 <alexraranda@gmail.com>
2021-02-02 16:26:51 -0800

    Fix file extensions, add menu to each page

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 import hero from "../assets/hero.jpeg";
+import hero2 from "../assets/hero2.jpg";
 import heroMobile from "../assets/hero_mobile.jpg";
+import heroMobile2 from "../assets/hero_mobile2.jpg";
+import { useLocation } from "react-router-dom";
 
-const Img = styled.img`
+const Img = styled.img<{ home: boolean }>`
   width: 100%;
-  height: 90vh;
-  background-image: url(${hero});
+  height: ${(props) => (props.home ? "90vh" : "30vh")};
+  background-image: url(${(props) => (props.home ? hero : hero2)});
   background-size: cover;
   background-attachment: fixed;
   @media screen and (max-width: 600px) {
-    background-image: url(${heroMobile});
+    background-image: url(${(props) =>
+      props.home ? heroMobile : heroMobile2});
   }
 `;
 
 export const Image = () => {
-  return <Img />;
+  const location = useLocation();
+  return <Img home={location.pathname === "/"} />;
 };
---
+++ src/routes/Contact.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Image } from "../components/Image";
+import Menu from "../components/Menu";
+
+const Contact = () => {
+  return (
+    <>
+      <Image />
+      <Menu />
+      <h1>CONTACT</h1>
+    </>
+  );
+};
+
+export default Contact;
